67428da137fe825a253818ce0d1f932453be96be
claudio <vicomser.claudio@gmail.com>
2024-04-29 17:50:12 -0300

    finalizo modularizacion de user

--- types/user.go
+++ types/user.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"fmt"
+
+	"github.com/bxcodec/faker/v3"
 	"github.com/google/uuid"
 )
 
@@ -34,4 +37,37 @@ func (user *User) SetId(Id int) *User {
 func (user *User) SetEmail(Email string) *User {
 	user.Email = Email
 	return user
+}
+
+func (user *User) PrintUsers(users *map[string]User) {
+	fmt.Println("----------------------------------------------------------------------------------------------------")
+	fmt.Printf("%-42s %-25s %-20s %-20s\n", "Id", "Email", "Firstname", "Lastname")
+	fmt.Println("----------------------------------------------------------------------------------------------------")
+
+	for item := range *users {
+		user := (*users)[item]
+		fmt.Printf("%-42s %-25s %-20s %-20s\n", user.Id, user.Email, user.Profile.GetName(), user.Profile.GetSurname())
+	}
+}
+
+func (user *User) AddNewUser(users *map[string]User, email string, firstname string, lastname string) {
+	newUser := NewUser(email)
+	newUser.Profile.SetName(firstname)
+	newUser.Profile.SetSurname(lastname)
+	(*users)[newUser.Id] = *newUser
+}
+
+func (user *User) CreateUsers(quantity int) *map[string]User {
+	users := make(map[string]User)
+
+	for i := 0; i < quantity; i++ {
+
+		newUser := NewUser(faker.Email())
+
+		newUser.Profile.SetName(faker.FirstName())
+		newUser.Profile.SetSurname(faker.LastName())
+
+		users[newUser.Id] = *newUser
+	}
+	return &users
 }
